19d801c00b90fc53988b6f89568170eec810f599
Jitendra More <jmore@DELC02VG0U1G8WN.sea.corp.expecn.com>
2019-07-01 19:13:09 +0530

    update

--- userdata.sh
+++ userdata.sh
@@ -3,9 +3,10 @@ set -x
 # output log of userdata to /var/log/user-data.log
 exec > >(tee /var/log/user-data.log|logger -t user-data -s 2>/dev/console) 2>&1
 
-yum install -y httpd
-service httpd start
-chkonfig httpd on
+sudo apt-get update
+sudo apt install apache2
+sudo ufw allow in "Apache Full"
+sudo chmod -R 0755 /var/www/html/
+sudo systemctl enable apache2
+sudo systemctl start apache2
 echo "<html><h1>Hello from Jitendra More - 9833550438</h2></html>" > /var/www/html/index.html
-
-
